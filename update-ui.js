d9789a436d12834596915edea94c3d8c498a0c46
flamewing <flamewing.sonic@gmail.com>
2025-05-06 11:25:08 +0200

    New hair menu

--- update-ui.js
+++ update-ui.js
@@ -27,15 +27,4 @@ function update() {
             image.style = "width: 0; margin: 0 0; opacity: 0; border-width: 0em;";
         }
     }
-    var hair_icons = document.getElementsByClassName("hair-icon");
-    console.log(hair_icons.length);
-    for (var i = 0; i < hair_icons.length; i++) {
-        var image = hair_icons[i];
-        var hair_match = image.getAttribute("data-hair") == hair;
-        if (hair_match) {
-            image.style = "opacity: 1;";
-        } else {
-            image.style = "opacity: 0;";
-        }
-    }
 }
